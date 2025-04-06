1e66fa039e9043797720124acc692ddf3dfdc570
Zenya <ee.zen@icloud.com>
2025-04-06 14:44:53 +0800

    Change default sample rate

--- websocket/asr_server.py
+++ websocket/asr_server.py
@@ -98,7 +98,7 @@ async def start():
     args.port = int(os.environ.get('VOSK_SERVER_PORT', 2700))
     args.model_path = os.environ.get('VOSK_MODEL_PATH', 'model')
     args.spk_model_path = os.environ.get('VOSK_SPK_MODEL_PATH')
-    args.sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 8000))
+    args.sample_rate = float(os.environ.get('VOSK_SAMPLE_RATE', 16000))
     args.max_alternatives = int(os.environ.get('VOSK_ALTERNATIVES', 0))
     args.show_words = bool(os.environ.get('VOSK_SHOW_WORDS', True))
     # 'phones' or 'words', see https://github.com/alphacep/vosk-api/pull/1377
